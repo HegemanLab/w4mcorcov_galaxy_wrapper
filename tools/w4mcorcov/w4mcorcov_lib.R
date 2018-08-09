5dd11b5cb23df8e199629db749ae5e4c8e520978
Art Eschenlauer <eschen@alumni.princeton.edu>
2018-08-09 23:29:43 +0000

    emit package info to stderr

--- tools/w4mcorcov/w4mcorcov_lib.R
+++ tools/w4mcorcov/w4mcorcov_lib.R
@@ -1,4 +1,10 @@
 suppressMessages(library(batch))
 # suppressMessages(library(foreach))
 suppressMessages(library(ropls))
-library(methods)
+suppressMessages(library(methods))
+
+cat("Installed packages:",stderr())
+write.table((installed.packages(.Library, priority = "high"))[, c(1,3:5)], stderr())
+cat("Loaded packages:",stderr())
+write(.packages(), stderr())
+

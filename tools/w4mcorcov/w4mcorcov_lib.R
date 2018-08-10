d35fe22e48375fa9ca86d7e3b83c1688f40010b5
Art Eschenlauer <eschen@alumni.princeton.edu>
2018-08-10 05:54:37 +0000

    debugging sessionInfo

--- tools/w4mcorcov/w4mcorcov_lib.R
+++ tools/w4mcorcov/w4mcorcov_lib.R
@@ -3,8 +3,10 @@ suppressMessages(library(batch))
 suppressMessages(library(ropls))
 suppressMessages(library(methods))
 
-cat("Installed packages:",stderr())
-write.table((installed.packages(.Library, priority = "high"))[, c(1,3:5)], stderr())
-cat("Loaded packages:",stderr())
-write(.packages(), stderr())
+# cat("Installed packages:",stderr())
+# write.table((installed.packages(.Library, priority = "high"))[, c(1,3:5)], stderr())
+# cat("Loaded packages:",stderr())
+# write(.packages(), stderr())
+
+print(sessionInfo())
 
